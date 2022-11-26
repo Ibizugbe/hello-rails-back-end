4c388904d1911a1c6b19770d8172a2ed1403122d
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-11-26 19:05:03 +0100

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
-    before_action :set_default_format
+  before_action :set_default_format
 
-    private
-  
-    def set_default_format
-      request.format = :json
-    end
+  private
+
+  def set_default_format
+    request.format = :json
+  end
 end
